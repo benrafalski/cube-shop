00af5d758b33d9a1d14e94744f2a9a0e8ede3371
benrafalski <79751177+benrafalski@users.noreply.github.com>
2021-08-16 18:24:43 -0700

    register styled component

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import '../styles.css'
 import axios from '../axios.js'
+import styled from 'styled-components'
 
 const Register = () => {
     const history = useHistory()
@@ -62,7 +63,277 @@ const Register = () => {
     }
 
     return (
-        <div className='register'>
+        <Reg>
+            <Link to='/' style={{ textDecoration:'none' }}>
+                <Logo>CUBEsTOR</Logo>
+            </Link>
+            <Container>
+            <h1>Register Account</h1>
+                <form>
+                    <h5>Firstname<Required>*</Required></h5>
+                    <input
+                        type='text' 
+                        data-lpignore='true' 
+                        value={firstname}
+                        onChange={e => setFirstname(e.target.value)}
+                    />
+                    <h5>Lastname<Required>*</Required></h5>
+                    <input
+                        type='text' 
+                        data-lpignore='true' 
+                        value={lastname}
+                        onChange={e => setLastname(e.target.value)}
+                    />
+                    <h5>Email<Required>*</Required></h5>
+                    <input
+                        type='text' 
+                        data-lpignore='true' 
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <h5>Username<Required>*</Required></h5>
+                    <input
+                        type='text' 
+                        data-lpignore='true' 
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}
+                    />
+                    <h5>Password<Required>*</Required></h5>
+                    <input
+                        type='password' 
+                        data-lpignore='true' 
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                    <h5>Verify Password<Required>*</Required></h5>
+                    <input
+                        type='password' 
+                        data-lpignore='true' 
+                        value={passwordVerify}
+                        onChange={e => setPasswordVerify(e.target.value)}
+                    />
+                    <RegisterButton
+                        type='submit'
+                        onClick={register} 
+                    >
+                        <span>Create new account</span>
+                    </RegisterButton>
+                </form>
+                <Agreement>
+                    By continuing, you agree to CUBEsTOR's Conditions of Use and Privacy Notice.
+                </Agreement>
+                <Option>
+                    &#160;Already have an account?&#160;
+                </Option>
+                <LoginButton onClick={signIn}>
+                    <span>Sign In</span>
+                </LoginButton>
+            </Container>
+        </Reg>
+    )
+}
+
+const Reg = styled.div`
+    display: flex;
+	flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    background-color: #fff;
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+	scroll-behavior: smooth;
+	background: linear-gradient(to right, #161616, #000000);
+`;
+const Logo = styled.h1`
+    background-color: #a8ff78;
+	background-image: linear-gradient(to top, #78ffd6 0%, #a8ff78 100%);
+	background-size: 100%;
+	-webkit-background-clip: text;
+	-moz-background-clip: text;
+	-webkit-text-fill-color: transparent;
+	-moz-text-fill-color: transparent;
+	display: flex;
+	align-items: center;
+	cursor: pointer;
+	text-decoration: none;
+	font-size: 2rem;
+	margin-top: 20px;
+	margin-bottom: 20px;
+`;
+const Container = styled.div`
+    width: 300px;
+    height: fit-content;
+    display: flex;
+    flex-direction: column;
+    border-radius: 5px;
+    border: 1px solid lightgray;
+    padding: 20px;
+	background: #131313;
+
+    h1 {
+        font-weight: 500;
+        margin-bottom: 20px;
+        color: #fff;
+        text-align: center;
+    }
+
+    form > h5 {
+        margin-bottom: 5px;
+        /* text-align: center; */
+        background: #da22ff;  /* fallback for old browsers */
+        background: -webkit-linear-gradient(to right, #9114ff, #da22ff);  /* Chrome 10-25, Safari 5.1-6 */
+        background: linear-gradient(to right, #8f0eff, #da22ff);
+        background-size: 100%;
+        -webkit-background-clip: text;
+        -moz-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        -moz-text-fill-color: transparent;
+    }
+
+    form > input {
+        height: 30px;
+        margin-bottom: 5px;
+        border-radius: 3px;
+        border-style: solid;
+        background-color: #111111;
+        width: 98%;
+        color: #fff;
+    }
+`;
+const Required = styled.span`
+    margin-bottom: 5px;
+	/* text-align: center; */
+	background: #fff;  /* fallback for old browsers */
+	background: -webkit-linear-gradient(to right, #fff, #fff);  /* Chrome 10-25, Safari 5.1-6 */
+	background: linear-gradient(to right, #fff, #fff);
+	background-size: 100%;
+	-webkit-background-clip: text;
+	-moz-background-clip: text;
+	-webkit-text-fill-color: transparent;
+	-moz-text-fill-color: transparent;
+`;
+const LoginButton = styled.button`
+    width: 100%;
+	height: 30px;
+	padding: 0 1rem;
+	border: none;
+	border-radius: 4px;
+	margin-top: 1rem;
+	cursor: pointer;
+	position: relative;
+	transition: all 0.35s;
+	outline: none;
+
+    span{
+        position: relative;
+        z-index: 2;
+        color: #111111;
+        text-decoration: none;
+    }
+
+    &:after{
+        position: absolute;
+        content: '';
+        top: 0;
+        left: 0;
+        width: 0;
+        height: 100%;
+        background: #777;
+        transition: all 0.35s;
+        border-radius: 4px;
+    }
+
+    &:hover{
+        color: #fff;
+    }
+
+    &:hover:after{
+        width: 100%;
+        height: 30px;
+    }
+`;
+const Agreement = styled.p`
+    margin-top: 15px;
+	text-align: center;
+    font-size: 12px;
+	background: #11998e;  /* fallback for old browsers */
+	background: -webkit-linear-gradient(to right, #38ef7d, #11998e);  /* Chrome 10-25, Safari 5.1-6 */
+	background: linear-gradient(to right, #38ef7d, #11998e); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+	background-size: 100%;
+	-webkit-background-clip: text;
+	-moz-background-clip: text;
+	-webkit-text-fill-color: transparent;
+	-moz-text-fill-color: transparent;
+`;
+const Option = styled.p`
+    display: flex;
+	flex-direction: row;
+	margin-top: 10px;
+    color: #fff;
+	background: #fff;  /* fallback for old browsers */
+	background-size: 100%;
+	-webkit-background-clip: text;
+	-moz-background-clip: text;
+	-webkit-text-fill-color: transparent;
+	-moz-text-fill-color: transparent;
+
+    &:before, &:after{
+        content: "";
+        flex: 1 1;
+        border-bottom: 2px solid;
+        border-color: #fff;
+        margin: auto;
+        padding: 1px;
+    }
+`;
+const RegisterButton = styled.button`
+    background: #59C173; 
+	background: -webkit-linear-gradient(to left, #5D26C1, #a17fe0, #59C173); 
+	background: linear-gradient(to left, #5D26C1, #a17fe0, #59C173);
+	width: 98%;
+	height: 30px;
+	padding: 0 1rem;
+	border: none;
+	border-radius: 4px;
+	margin-top: 1rem;
+	cursor: pointer;
+	position: relative;
+	transition: all 0.35s;
+	outline: none;
+
+    span{
+        position: relative;
+        z-index: 2;
+        color: #fff;
+        text-decoration: none;
+    }
+
+    &:after{
+        position: absolute;
+        content: '';
+        top: 0;
+        left: 0;
+        width: 0;
+        height: 100%;
+        background: #00F260;
+        transition: all 0.35s;
+        border-radius: 4px;
+    }
+
+    &:hover{
+        color: #fff;
+    }
+
+    &:hover:after{
+        width: 100%;
+        height: 30px;
+    }
+`;
+
+//old 
+/*  
+<div className='register'>
             <Link to='/' style={{ textDecoration:'none' }}>
                 <h1 className='register__logo'>CUBEsTOR</h1>
             </Link>
@@ -128,8 +399,6 @@ const Register = () => {
                     <span>Sign In</span>
                 </button>
             </div>
-        </div>
-    )
-}
+        </div>*/
 
 export default Register
